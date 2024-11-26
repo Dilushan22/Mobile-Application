251a7b7d2cf52033785f5cbe4e80da6a58ff1426
Dilushan Gnanaraja <diluboii@gmail.com>
2024-11-26 15:41:19 +0400

    Webstore Application

---
+++ products.js
@@ -0,0 +1,38 @@
+let products = [
+    {
+        "id":1001,
+        "title":"Cat Food, 25lb bag",
+        "description":"A 25 pound bag of <em>irresistible</em>,"+"oragnic goodness for your cat.",
+        "price": 20.00,
+        "image": "Images/Product01img.jpeg",
+        "availableInventory": 10,
+        "rating": 3,
+    },
+    {
+        "id":1002,
+        "title":"Yarn",
+        "description":"yarn your cat could play with for a very <strong>long</strong> time.",
+        "price": 2.99,
+        "image": "Images/images.jpg",
+        "availableInventory": 7,
+        "rating": 2,
+    },
+    {
+        "id":1003,
+        "title":"Kitty Litter",
+        "description":"<strong>Premium</strong> kitty litter for your cat.",
+        "price": 7.50,
+        "image": "Images/cat litter.jpg",
+        "availableInventory": 9,
+        "rating": 4,
+    },
+    {
+        "id":1004,
+        "title":"Kitty cot",
+        "description":"<strong>Premium</strong> kitty cot for your cat.",
+        "price": 10.00,
+        "image": "Images/71DPp5p9NKL._AC_UF894,1000_QL80_.jpg",
+        "availableInventory":8,
+        "rating": 3,
+    },
+]
